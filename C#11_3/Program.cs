8aafdbd743a2e19b2d192320261c7c59fd6e0ff8
Grinchs <marcisg2345@gmail.com>
2024-05-13 23:04:34 +0300

    Add project files.

---
+++ C#11_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_11_3
+{
+    internal class Program
+    {
+        class Konstruktors
+        {
+            public Konstruktors()
+            {
+                Console.WriteLine("Default konstruktors");
+            }
+            public Konstruktors(int i)
+            {
+                Console.WriteLine($"int tipa parametrs:{i}");  
+            }
+            public Konstruktors(double d)
+            {
+                Console.WriteLine($"double tipa parametrs: {d}");
+            }
+            public Konstruktors(string s)
+            {
+                Console.WriteLine($"string tipa parametrs: {s}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Konstruktors konstruktors = new Konstruktors();    
+            Konstruktors i = new Konstruktors(3);
+            Konstruktors d = new Konstruktors(1.1);
+            Konstruktors s = new Konstruktors("h");    
+        }
+    }
+}
